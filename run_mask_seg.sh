cc512ac6772c25e8d8e9fc52bdd37f81b5545610
Yuyan Chen <142872743+Chenyuyann@users.noreply.github.com>
2023-12-08 19:52:51 +0800

    Update run_mask_seg.sh

--- run_mask_seg.sh
+++ run_mask_seg.sh
@@ -1,6 +1,7 @@
 cp -r /home/data/2788/ /project/train/src_repo/
 mkdir -p /project/train/src_repo/yolov8/datasets/labels/train
 mkdir /project/train/src_repo/yolov8/datasets/labels/val
+mv /project/train/src_repo/instances-seg.yaml /project/train/src_repo/yolov8/datasets
 
 cd /project/train/src_repo/
 python split.py
