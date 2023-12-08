d25c4d48b6980eb815039f451bf1f355d8a97711
Yuyan Chen <142872743+Chenyuyann@users.noreply.github.com>
2023-12-08 19:36:17 +0800

    Update train-seg.py

--- train-seg.py
+++ train-seg.py
@@ -1,11 +1,9 @@
-'''
-实例分割训练
-'''
 from ultralytics import YOLO
 
 # train
-model = YOLO('/project/train/src_repo/yolov8/models/yolov8n-seg.pt')  # build from YAML and transfer weights
+model = YOLO('/project/train/src_repo/yolov8/models/yolov8n-seg.pt') # 官方的预训练模型
+# model = YOLO('/project/train/models/train2/weights/last.pt')  # 接着上次的模型训练
 
 # Train the model
 model.train(data='/project/train/src_repo/yolov8/datasets/instances-seg.yaml',  
-            project = '/project/train/models', epochs=10, imgsz=1920, batch=4)
+            project = '/project/train/models', epochs=150, imgsz=1080, batch=8)
