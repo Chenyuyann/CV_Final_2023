180e447866f889f848bfda5e16ac414ee06072bf
Yuyan Chen <142872743+Chenyuyann@users.noreply.github.com>
2023-12-08 19:29:30 +0800

    Update test-seg.py

--- test-seg.py
+++ test-seg.py
@@ -1,5 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("/project/train/models/train/weights/best.pt")
-results = model.predict(source="datasets/images/val", save=False)
-results.save("/project/train/result-graphs")
+# train
+model = YOLO('/project/train/src_repo/yolov8/models/yolov8n-seg.pt') # 官方的预训练模型
+# model = YOLO('/project/train/models/train2/weights/last.pt')  # 接着上次的模型训练
+
+# Train the model
+model.train(data='/project/train/src_repo/yolov8/datasets/instances-seg.yaml',  
+            project = '/project/train/models', epochs=150, imgsz=1080, batch=8)
