db266fe1acbfefa2bcc2be22803720a2588be006
Yuyan Chen <142872743+Chenyuyann@users.noreply.github.com>
2023-12-08 19:37:25 +0800

    Delete test-seg.py

--- test-seg.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# train
-model = YOLO('/project/train/src_repo/yolov8/models/yolov8n-seg.pt') # 官方的预训练模型
-# model = YOLO('/project/train/models/train2/weights/last.pt')  # 接着上次的模型训练
-
-# Train the model
-model.train(data='/project/train/src_repo/yolov8/datasets/instances-seg.yaml',  
-            project = '/project/train/models', epochs=150, imgsz=1080, batch=8)
